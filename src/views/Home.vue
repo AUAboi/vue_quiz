024a7e42f34a16b16f59ab93564de96f3f2b22ff
Ahsan AUA <auawdigital@gmail.com>
2020-12-22 21:34:13 +0500

    Basic project setup

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<section class="">
+			<h1>QUIZ</h1>
+			<h2>Game</h2>
+			<div>
+				<router-link to="/rules">Rules</router-link>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Home"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Rules.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>This is Rules Page</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "About"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
